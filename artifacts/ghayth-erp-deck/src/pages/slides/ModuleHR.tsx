f8b7f593f67d3bb9aa8c32437f89436fae50485c
barhom64 <barhom64@gmail.com>
2026-04-25 00:31:38 +0300

    sync: artifacts/ghayth-erp-deck/src/pages/slides/ModuleHR.tsx

--- artifacts/ghayth-erp-deck/src/pages/slides/ModuleHR.tsx
+++ artifacts/ghayth-erp-deck/src/pages/slides/ModuleHR.tsx
@@ -1,5 +1,6 @@
 import { HRIcon } from "../../components/ModuleIcons";
 import PlatformShot from "../../components/PlatformShot";
+import DeepLinkButton from "../../components/DeepLinkButton";
 
 export default function ModuleHR() {
   return (
@@ -55,6 +56,12 @@ export default function ModuleHR() {
         </div>
       </div>
 
+      <DeepLinkButton
+        to="/deep/slide2"
+        label="تعمّق في هذه الوحدة"
+        variant="to-deep"
+        className="absolute bottom-[2.6vh] right-[6vw]"
+      />
       <div className="absolute bottom-[3vh] left-[6vw] text-muted font-body text-[1vw]">05 / 19</div>
     </div>
   );
