457fa41e54a8cce4f704a9c24025933325ed2a07
barhom64 <barhom64@gmail.com>
2026-04-25 00:28:54 +0300

    sync: artifacts/api-server/src/app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -89,8 +89,8 @@ app.use(cors({
   },
   credentials: true,
 }));
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use(eventBusMiddleware);
 app.use(auditMiddleware);
